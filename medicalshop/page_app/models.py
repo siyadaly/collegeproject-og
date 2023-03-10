fca8e22052b61495423396fcb2e8caf04f9e8610
siya <siyadaly19@gmail.com>
2023-03-10 21:10:53 +0530

    adding changes

--- medicalshop/page_app/models.py
+++ medicalshop/page_app/models.py
@@ -3,35 +3,35 @@ from django.db import models
 
 # Create your models here.
 class staff(models.Model):
-    staff_name =models.varchar(255)
-    email =models.varchar(255)
-    password =models.varchar(255)
-    staff_phonenumber = models.varchar(10)
+    staff_name = models.TextField()
+    email =models.EmailField()
+    password =models.TextField()
+    staff_phonenumber = models.IntegerField()
 
 class product(models.Model):
-    product_name = models.varchar(255)
-    cost = models.varchar(255)
-    type = models.varchar(255)
+    product_name = models.TextField()
+    cost = models.IntegerField()
+    type = models.TextField()
     product_Description = models.TextField()
 
 class order(models.Model):
-    productid = models.int()
-    order_name = models.varchar(255)
-    customer_id = models.int()
-    created_on = models.DateTimeField(auto_now=true, auto_now_add=true) 
+    productid = models.IntegerField()
+    order_name = models.TextField()
+    customer_id = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True) 
 
 class cutomer(models.Model):
-    cus_email = models.varchar(255)
-    cus_name = models.varchar(255)
-    cus_password = models.varchar(255)
-    cus_phonenumber = models.varchar(10)
-    cus_address = models.varchar(255)
+    cus_email = models.EmailField(max_length=254)
+    cus_name = models.TextField()
+    cus_password = models.TextField()
+    cus_phonenumber =models.IntegerField()
+    cus_address = models.TextField()
 
 class payment(models.Model):
-    order_id = models.varchar(255)
-    cus_id = models.varchar(255)
-    cus_name = models.varchar(255)
-    cus_address = models.varchar(255)
+    order_id = models.IntegerField()
+    cus_id = models.IntegerField()
+    cus_name = models.TextField()
+    cus_address = models.TextField()
     product_description = models.TextField()
 
 
